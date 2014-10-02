db82efa0d5ab7dc6f4b6ef8305080e7b6f438ea3
unknown <jwderry@L-R9ELYF6.eads.ncads.net>
2014-10-02 00:34:46 -0400

    inital Chapter6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapter6::Application.config.secret_token = '27e0c8561f856c50e9379e1e98a50c9a1a8bca1ef9b5d105b115fa9fb670ff3fdce40e2f160faf95d3b595bebb87466cb671907d23d0f2b9f5fb9b73ae008e2c'
